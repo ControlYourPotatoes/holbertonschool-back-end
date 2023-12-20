3013c7ce228cd73a833a8c74f33d527b405ec8c2
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-12-20 17:13:28 -0400

    document

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """
-This script fetches data from a REST API for a given employee
-and exports the data in the CSV format.
+This script fetches data from a REST API for  employeeID, 
+prints the employee's TODO list progress,
+and exports the data to a CSV file.
+
+Usage: python3 1-export_to-CSV.py [employee_id]
+
 """
 import csv
 import requests
