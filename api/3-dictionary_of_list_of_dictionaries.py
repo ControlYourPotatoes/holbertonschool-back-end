4f8b9925c5e56940bb9a69d0eca7561be719db35
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-12-20 18:37:00 -0400

    3-dictonary

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import json
+import requests
+
+def fetch_data():
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    users = requests.get(users_url).json()
+    tasks = requests.get(todos_url).json()
+
+    user_task_dict = {}
+
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+
+        user_task_dict[user_id] = [
+            {
+                "username": username,
+                "task": task['title'],
+                "completed": task['completed']
+            }
+            for task in tasks if task['userId'] == user_id
+        ]
+
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(user_task_dict, jsonfile)
+
+if __name__ == "__main__":
+    fetch_data()
